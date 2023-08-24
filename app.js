c87cd24d2731d6da1af41dc3a23170b918ba7a23
Vidar Kodehode <125680728+VidarHeritier@users.noreply.github.com>
2023-08-24 15:29:41 +0200

    Some UX changes

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const req = new XMLHttpRequest();
-const url = "https://api.nasa.gov/planetary/apod?api_key=";
+let url = "https://api.nasa.gov/planetary/apod?api_key=";
 const api_key = "rRqeqR1QzYIucfDqchocMM7KurKMjvgYLqwimwj9";
+const calendar = document.querySelector("input");
+const loadingAnim = document.querySelector("loading");
 
-const apiUrl = url + api_key;
+let apiUrl = url + api_key;
+
+let initialLoad = true;
 
 async function fetchNasaData() {
   try {
-    const response = await fetch(apiUrl);
+    let response = await fetch(apiUrl);
     const data = await response.json();
+
     return [data];
   } catch (error) {
     console.error("Error loading data", error);
@@ -16,7 +21,17 @@ async function fetchNasaData() {
 }
 
 fetchNasaData().then((nasaArray) => {
-  console.log(nasaArray);
+  const loadingAnim = document.querySelector(".loading");
+
+  // loadingAnim.onreadystatechange = () => {
+  //   if (document.readyState !== "complete") {
+  //     loadingAnim.style.display = "flex";
+  //     console.log("ikke");
+  //   } else {
+  //     loadingAnim.style.display = "none";
+  //     console.log("jq");
+  //   }
+  // };
 
   let today = new Date().toISOString().slice(0, 10);
 
@@ -31,8 +46,21 @@ fetchNasaData().then((nasaArray) => {
   const date = document.createElement("h3");
   date.textContent = POD.date;
 
+  const headerContainer = document.createElement("div");
+  headerContainer.className = "header-container";
+  headerContainer.append(head, title, date);
+
   const image = document.createElement("img");
+  image.className = "nasa-img";
   image.src = POD.url;
+
+  image.addEventListener("click", function () {
+    image.requestFullscreen();
+  });
+
+  image.addEventListener("dblclick", function () {
+    document.exitFullscreen();
+  });
 
   const dateSelectorHeading = document.createElement("h4");
   dateSelectorHeading.textContent = "Select a date in time";
@@ -40,16 +68,16 @@ fetchNasaData().then((nasaArray) => {
   const dateSelectorContainer = document.createElement("div");
 
   const leftArrow = document.createElement("button");
-  leftArrow.textContent = "backwards";
+  leftArrow.textContent = "backward";
 
-  const calendar = document.createElement("input");
+  let calendar = document.createElement("input");
   calendar.type = "date";
   calendar.min = "1996-01-01";
   calendar.max = today;
   calendar.value = today;
 
   const rightArrow = document.createElement("button");
-  rightArrow.textContent = "forwards";
+  rightArrow.textContent = "forward";
 
   const description = document.createElement("p");
   description.textContent = POD.explanation;
@@ -59,12 +87,64 @@ fetchNasaData().then((nasaArray) => {
   document
     .querySelector("body")
     .append(
-      head,
-      title,
+      headerContainer,
       image,
       dateSelectorHeading,
       dateSelectorContainer,
-      date,
       description
     );
+
+  function setNewDate(offset) {
+    const currentDate = new Date(calendar.value);
+    currentDate.setDate(currentDate.getDate() + offset);
+    if (currentDate > new Date(calendar.max)) {
+      console.error("Legg inn error på et vis");
+      return;
+    }
+    const newDate = currentDate.toISOString().slice(0, 10);
+    calendar.value = newDate;
+
+    if (!initialLoad) {
+      const rect = image.getBoundingClientRect();
+      console.log(rect);
+      loadingAnim.style.top = rect.top + image.offsetHeight / 2;
+      loadingAnim.style.right = rect.right + image.offsetWidth / 2;
+
+      loadingAnim.style.display = "flex";
+    }
+
+    upDate(newDate);
+  }
+
+  function upDate(selectedDate) {
+    apiUrl = `https://api.nasa.gov/planetary/apod?date=${selectedDate}&api_key=${api_key}`;
+
+    fetchNasaData().then((nasaArray) => {
+      const POD = nasaArray[0];
+
+      title.textContent = POD.title;
+      date.textContent = POD.date;
+      image.src = POD.url;
+      description.textContent = POD.explanation;
+
+      loadingAnim.style.display = "none";
+      if (initialLoad) {
+        initialLoad = false;
+      }
+    });
+  }
+
+  leftArrow.addEventListener("click", function () {
+    setNewDate(-1);
+  });
+
+  rightArrow.addEventListener("click", function () {
+    setNewDate(1);
+  });
+
+  calendar.addEventListener("change", function () {
+    setNewDate(0);
+  });
 });
+
+//-----------------------------------------------------------------------
